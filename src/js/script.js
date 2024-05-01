d551005d5339def6f218552c84a98f1b24aa4df3
Muhamadfaruqosama <abdolslamet@gmail.com>
2024-05-01 08:52:07 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+// sticky navbar
+window.addEventListener('scroll', function() {
+    let navbar = document.getElementById('navbar');
+    let sticky = navbar.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        navbar.classList.remove('top');
+        navbar.classList.add('shadow');
+    } else {
+        navbar.classList.remove('shadow');
+        navbar.classList.add('top');
+    }
+});
+
+// AOS init
+AOS.init();
+
+// parallax
+var scene = document.getElementById('landing-page');
+var parallaxInstance = new Parallax(scene);
+
+// back button
+function goBack() {
+    window.history.back();
+  }
